998faa09b73fa7506b4526e1128fa5c75084eeb6
Tushar <tusharkhairnar6007@gmail.com>
2025-07-29 22:13:22 +0530

    tcart_area_category_branch

---
+++ MartConnect/server/src/main/java/com/sunbeam/entities/Category.java
@@ -0,0 +1,37 @@
+package com.sunbeam.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+    
+    @NotBlank(message = "Category name is required")
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+    
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+    
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+} 
